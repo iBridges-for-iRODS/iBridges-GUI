515c343f2469c7b5f1e0254c705dd504a426568e
chstaiger <staigerchristine@gmail.com>
2024-05-16 10:10:44 +0200

    BIgfix and starting implementation of  download

--- ibridgesgui/search.py
+++ ibridgesgui/search.py
@@ -41,7 +41,8 @@ class Search(PyQt6.QtWidgets.QWidget, Ui_tabSearch):
         self.hide_result_elements()
         self.searchButton.clicked.connect(self.search)
         self.clearButton.clicked.connect(self.clear)
-        
+        self.downloadButton.clicked.connect(self.download)
+
         #group textfields for gathering key, value, unit
         self.keys = [self.key1, self.key2, self.key3, self.key4]
         self.vals = [self.val1, self.val2, self.val3, self.val4]
@@ -76,28 +77,29 @@ class Search(PyQt6.QtWidgets.QWidget, Ui_tabSearch):
         self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.WaitCursor))
         self.errorLabel.clear()
         
-        if self._check_search_params():
+        if self._check_keyvals():
             # search parameters
-            if not all(key == '' for key in self.keys):
+            if all([key.text() == "" for key in self.keys]):
+                key_vals = None
+            else:
                 key_vals = dict(zip([key.text() for key in self.keys],
                                     [val.text() for val in self.vals]))
-            else:
-                key_vals = None
-
-            path = self.path_field.text() if self.path_field.text() is not None else None
-            checksum = self.checksum_field.text() if self.checksum_field.text() is not None \
+            
+            path = self.path_field.text() if self.path_field.text() != "" else None
+            checksum = self.checksum_field.text() if self.checksum_field.text() != "" \
                                                   else None
-            print(key_vals)
-            print(path)
-            print(checksum)
-
-            results = search_data(self.session, path=path, checksum=None, key_vals=None)
-            print(results)
-            if len(results) == 0:
-                self.errorLabel.setText("No objects or collections found.")
-            else: 
-                self.show_result_elements()
-                self.load_results(results)
+            
+            print(f"path:{path == None}, checksum: {checksum == None}, keyvals: {key_vals}")
+            if key_vals == None and path == None and checksum == None:
+                self.errorLabel.setText("No search critera given.")
+            else:
+                results = search_data(self.session, path=path, checksum=checksum,
+                                      key_vals=key_vals)
+                if len(results) == 0:
+                    self.errorLabel.setText("No objects or collections found.")
+                else: 
+                    self.show_result_elements()
+                    self.load_results(results)
         self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.ArrowCursor))
 
     def load_results(self, results):
@@ -116,12 +118,53 @@ class Search(PyQt6.QtWidgets.QWidget, Ui_tabSearch):
                                   coll.modify_time.strftime("%d-%m-%Y")))
             populate_table(self.searchResTable, len(table_data), table_data)
 
+    def download(self):
+        
+        info = ""
+        
+        # Get multiple paths from table
+        irods_paths = [] 
+        for idx in self.searchResTable.selectedIndexes():
+            irods_paths.append(IrodsPath(
+                                    self.session, 
+                                    self.searchResTable.item(idx.row(), 0).text(),
+                                    self.searchResTable.item(idx.row(), 1).text()))
+
+        select_dir = Path(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Direcectory"))
+        if select_dir == "":
+            return
+        else:
+            info = f"Download to: {select_dir}\n"
+
+        data_exists = [(ipath, local_path.joinpath(ipath.name).exists()) for ipath in irods_paths]
+        overwrite = False
+
+        if any(exists for _, exists in data_exists): 
+            buttonReply = QMessageBox.question(self, "Message Box",
+                                               info+"Some data already exists. Overwrite data?")
+        else:
+            buttonReply = QMessageBox.question(self, "Message Box", info)
+
+        if buttonReply == QMessageBox.StandardButton.Yes:
+            overwrite = True
+            for ipath in irods_paths:
+                if ipath.exists():
+                    download(self.session, ipath, select_dir, overwrite=overwrite)
+                    logger.info("Downloading %s to %s, overwrite %s",
+                                str(ipath), select_dir, overwrite)
+                else:
+                    self.errorLabel.text(f"{str(ipath)} does not exist.")
+                    logger.info("Download failed: %s does nor exist", str(ipath))
+
+
     def fill_info(self):
         print("not implemented")
 
-    def _check_search_params(self):
-        for key, val in zip([key.text() for key in self.keys], [val.text() for val in self.vals]):
+    def _check_keyvals(self):
+        key_vals = zip([key.text() for key in self.keys], [val.text() for val in self.vals])
+        for key, val in key_vals:
             if key == "" and val != "":
                 self.errorLabel.setText("There are metadata values without key. Stop search.")
                 return False
+
         return True
