3a49cc54d97bd9a15686f87ac322bbfcc91509f1
chstaiger <staigerchristine@gmail.com>
2024-06-25 11:55:00 +0200

    Remove print statements

--- ibridgesgui/irods_tree_model.py
+++ ibridgesgui/irods_tree_model.py
@@ -39,7 +39,6 @@ class IrodsTreeModel(PyQt6.QtGui.QStandardItemModel):
         self.tree_view = tree_view
         self.session = irods_root_path.session
         self.irods_root_path = irods_root_path
-        print(f"iRODS Tree: constructor {self.irods_root_path}")
         # Empty tree
         self.clear()
 
@@ -63,8 +62,6 @@ class IrodsTreeModel(PyQt6.QtGui.QStandardItemModel):
             PyQt6.QtGui.QStandardItem(datatype),  # C or d
             PyQt6.QtGui.QStandardItem(item.path),  # absolute irods path
         ]
-        print(f"iRODS Tree: Insert row data: \n\t display: {display} \n\t item.path: {item.path}")
-        print(f"\t \t tree level: {str(level + 1)}, datatype {datatype}, item.path {item.path}")
         return row
 
     def init_tree(self):
@@ -74,7 +71,6 @@ class IrodsTreeModel(PyQt6.QtGui.QStandardItemModel):
 
         # Start the tree, add the highest level to the invisible root
         root_coll = IrodsPath(self.session, self.irods_root_path).collection
-        print(f"iRODS Tree init_tree: root_coll.path {root_coll.path}")
         root_row = self._tree_row_from_irods_item(root_coll, -1, -1, True)
         root.appendRow(root_row)
 
@@ -106,15 +102,12 @@ class IrodsTreeModel(PyQt6.QtGui.QStandardItemModel):
             [display_name, level, id, parent_id, 'C/d', absolute iRODS Path]
 
         """
-        print(f"iRODS Tree add subtree: tree_item_data {tree_item_data}")
         _, level, _, _, _, abs_irods_path = tree_item_data
         parent_coll = IrodsPath(self.session, abs_irods_path).collection
-        print(f"iRODS Tree add subtree: parent_coll {parent_coll}")
 
         # the irods root also contains the irods root as subcollection
         subcolls = [c for c in parent_coll.subcollections if c.path != "/"]
         dataobjs = parent_coll.data_objects
-        print([c.path for c in subcolls])
         # we assume that tree_item has no children yet.
         new_nodes = {}
         for item in subcolls + dataobjs:
