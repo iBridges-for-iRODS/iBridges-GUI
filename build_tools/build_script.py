410f58824cf7938dbc9bb6541401591dec7d8d0b
Daalen <tim.vandaalen@wur.nl>
2025-02-07 16:05:14 +0100

    converted all globals into arguments

--- build_tools/build_script.py
+++ build_tools/build_script.py
@@ -2,18 +2,9 @@
 from pathlib import Path
 from platform import system
 from shutil import copytree, rmtree
+import argparse
 
 import ui_to_py as uipy
-
-# Build config and file paths
-code_dir = Path("ibridgesgui")
-icons_folder = Path.cwd().joinpath(code_dir, 'icons')
-ui_folder = Path.cwd().joinpath(code_dir, 'ui_files')
-venv = Path.cwd().joinpath('venv')
-# Debug mode shows the console (command prompt with the logging)
-debug_mode = True
-remove_venv = False
-# ---------------------------------------------------------#
 
 
 def replace_folder(source: Path | str, destination: Path | str):
@@ -21,27 +12,30 @@ def replace_folder(source: Path | str, destination: Path | str):
     copytree(source, destination, dirs_exist_ok=True)
 
 
-def create_exe():
-    """Create the exe file for the application."""
+def create_exe(args):
+    """Create the exe file for the application.
+        args : argparse.Namespace
+            arguments passed to the function
+    """
     (python, cmd_sep) = uipy.os_specific_settings()
 
     # Step 1: Setup
     # Step 1a,(optional) remove the venv to prevent access issues.
-    if remove_venv:
-        rmtree(venv, ignore_errors=True)
-    venv.mkdir(exist_ok=True)
+    if args.rem_venv:
+        rmtree(args.venv, ignore_errors=True)
+    args.venv.mkdir(exist_ok=True)
 
     # Step 1b, Create the venv if needed
     # windows
     if "windows" in system().lower():
-        venv_activate = venv.joinpath('Scripts', 'activate.bat')
+        venv_activate = args.venv.joinpath('Scripts', 'activate.bat')
     else:  # Ubuntu/IOS
-        venv_activate = venv.joinpath('bin', 'activate')
+        venv_activate = args.venv.joinpath('bin', 'activate')
     if (not venv_activate.exists()) or (not venv_activate.is_file()):
         venv_activate = f"\"{str(venv_activate)}\""
         if "windows" not in system().lower():
             venv_activate = f"source {venv_activate}"
-        uipy.run_cmd(f"{python} -m venv {venv}")
+        uipy.run_cmd(f"{python} -m venv {args.venv}")
         uipy.run_cmd(f"{venv_activate} {cmd_sep} python -m pip install --upgrade pip")
         uipy.run_cmd(f"{venv_activate} {cmd_sep} pip install .")
         uipy.run_cmd(f"{venv_activate} {cmd_sep} pip install .[deploy]")
@@ -50,24 +44,24 @@ def create_exe():
 
     # Step 2 Convert .ui files to .py files
     # Recompiling is the best way to ensure they are up to date
-    uipy.remove_pyui_files(ui_folder)
-    uipy.ui_to_py(ui_folder, venv_activate, cmd_sep)
+    uipy.remove_pyui_files(args.ui_folder)
+    uipy.ui_to_py(args.ui_folder, venv_activate, cmd_sep)
 
     # Step 3, Activate venv and run nuitka
     cmd = f"{venv_activate} {cmd_sep} python -m nuitka "
-    if not debug_mode:
+    if not args.debug_exe:
         cmd += "--disable-console "
     cmd += f"--standalone --include-package=irods --nofollow-import-to=irods.test\
         --remove-output --enable-plugin=pyside6 \
         --assume-yes-for-downloads --show-progress  \
-        --windows-icon-from-ico=\"{icons_folder.joinpath('iBridges.ico')}\" \
-        {code_dir.joinpath('__main__.py')} --quiet"
+        --windows-icon-from-ico=\"{args.icons_folder.joinpath('iBridges.ico')}\" \
+        {args.code_folder.joinpath('__main__.py')} --quiet"
     uipy.run_cmd(cmd)
 
     # Step 4, move the icons folder to the distribution folder
-    replace_folder(icons_folder,
-                   icons_folder.parent.parent.joinpath('__main__.dist',
-                                                       icons_folder.name))
+    replace_folder(args.icons_folder,
+                   args.icons_folder.parent.parent.joinpath('__main__.dist',
+                                                            args.icons_folder.name))
 
     # Step 5, rename the distribution folder and file
     shipping_folder = Path('output/ibridgesgui')
@@ -81,4 +75,18 @@ def create_exe():
 
 
 if __name__ == "__main__":
-    create_exe()
+    default_code_folder = Path.cwd().joinpath('ibridgesgui')
+    Path.cwd()
+    parser = argparse.ArgumentParser(description="iBridges-Gui exe creator.")
+    parser.add_argument('--debug_exe', action="store_true", help='Build executable with debug console')
+    parser.add_argument('--rem_venv', action="store_true", help='remove virtual environment')
+    parser.add_argument('--code_folder', default=default_code_folder, type=Path,
+                        help='Full path to the directory with code')
+    parser.add_argument('--ui_folder', default=default_code_folder.joinpath('ui_files'), type=Path, required=False,
+                        help='Full path to the directory with ui files')
+    parser.add_argument('--icons_folder', default=default_code_folder.joinpath('icons'), type=Path, required=False,
+                        help='Full path to the directory with the icons')
+    parser.add_argument('--venv', default=Path.cwd().joinpath('venv'), type=Path, required=False,
+                        help='Full path to virtual python environment')
+    args = parser.parse_args()
+    create_exe(args)
