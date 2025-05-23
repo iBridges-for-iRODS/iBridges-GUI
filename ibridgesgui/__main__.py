e0e4987b4143396c601623e591edc8c3f547d637
Staiger, Christine <staigerchristine@gmail.com>
2025-05-23 08:55:32 +0200

    fix pylint and ruff

--- ibridgesgui/__main__.py
+++ ibridgesgui/__main__.py
@@ -307,7 +307,7 @@ def main_deprecated():
     """Deprecate the ibridges-gui call."""
     logger = logging.getLogger(THIS_APPLICATION)
     logger.warning(
-        "The command 'ibridges-gui' will be deprecated in iBridges 2.0. Use 'ibridges gui' instead.")
+        "The command 'ibridges-gui' will be deprecated in iBridges 2.0. Use 'ibridges gui' instead.") # noqa: E501
 
 if __name__ == "__main__":
     main()
