33f2c3bf49cc0f51a4b9147017203332dacb7326
chstaiger <staigerchristine@gmail.com>
2026-04-22 09:42:24 +0200

    refactor threads

--- ibridgesgui/threads.py
+++ ibridgesgui/threads.py
@@ -3,121 +3,104 @@
 from pathlib import Path
 
 import PySide6.QtCore
+from PySide6.QtCore import QThread, Signal
 from ibridges import IrodsPath, Session, search_data, sync
 from ibridges.executor import Operations, _obj_get, _obj_put
 from irods.exception import CAT_NO_ACCESS_PERMISSION, NetworkException
 
+class BaseThread(QThread):
+    """Base class for all iRODS worker threads."""
 
-class SearchThread(PySide6.QtCore.QThread):
-    """Start iRODS search in an own thread using the same iRODS session."""
+    result = Signal(dict)
 
-    result = PySide6.QtCore.Signal(dict)
+    def __init__(self, logger, ienv_path: Path):
+        super().__init__()
+        self.logger = logger
+        self.thread_session = Session(irods_env=ienv_path)
+        self.logger.debug(f"{self.__class__.__name__}: Created new session")
+
+    def cleanup_session(self):
+        """Close the session and log the result."""
+        self.thread_session.close()
+        if self.thread_session.irods_session is None:
+            self.logger.debug(f"{self.__class__.__name__}: Session successfully deleted.")
+        else:
+            self.logger.debug(f"{self.__class__.__name__}: Session still exists.")
+
+    def emit_error(self, message: str):
+        """Emit an error result."""
+        self.result.emit({"error": message})
+
+class SearchThread(BaseThread):
+    """Start iRODS search in a separate thread."""
+
+    result = Signal(dict)
 
     def __init__(
         self,
         logger,
         ienv_path: Path,
-        search_path: IrodsPath,
+        search_path,
         path_pattern: str,
         meta_searches: list,
         checksum: str,
         case_sensitive: bool,
-        item_type: str
+        item_type: str,
     ):
-        """Pass searh parameters."""
-        super().__init__()
-
-        self.logger = logger
-        self.thread_session = Session(irods_env=ienv_path)
-        self.logger.debug("Search thread: created new session")
-        self.sync_thread = None
+        super().__init__(logger, ienv_path)
         self.search_path = search_path
         self.path_pattern = path_pattern
+        self.meta_searches = meta_searches
         self.checksum = checksum
         self.case_sensitive = case_sensitive
-        self.ms = meta_searches
         self.item_type = item_type
 
-    def _delete_session(self):
-        self.thread_session.close()
-        if self.thread_session.irods_session is None:
-            self.logger.debug("Search thread: Thread session successfully deleted.")
-        else:
-            self.logger.debug("Search thread: Thread session still exists.")
-
     def run(self):
-        """Run the thread."""
-        search_out = {}
         try:
+            from ibridges import search_data
             res = search_data(
                 self.thread_session,
                 path=self.search_path,
                 path_pattern=self.path_pattern,
                 checksum=self.checksum,
-                metadata=self.ms,
+                metadata=self.meta_searches,
                 case_sensitive=self.case_sensitive,
-                item_type = self.item_type
+                item_type=self.item_type,
             )
-            # convert IrodsPaths to strings, the session  will be destroyed at the end of the thread
-            search_out["results"] = [str(ipath) for ipath in res]
-            self._delete_session()
-        except NetworkException:
-            self._delete_session()
-            search_out["error"] = "Search takes too long. Please provide more parameters."
-        self.result.emit(search_out)
+            results = [str(ipath) for ipath in res]
+            self.result.emit({"results": results})
 
+        except Exception as exc:
+            self.logger.exception("Search failed: %s", repr(exc))
+            self.emit_error("Search failed or took too long. Please refine your parameters.")
 
-class TransferDataThread(PySide6.QtCore.QThread):
+        finally:
+            self.cleanup_session()
+
+class TransferDataThread(BaseThread):
     """Transfer data between local and iRODS."""
 
-    result = PySide6.QtCore.Signal(dict)
-    current_progress = PySide6.QtCore.Signal(list)
+    current_progress = Signal(list)
 
-    def __init__(self, ienv_path: Path, logger, ops: Operations, overwrite: bool):
-        """Pass parameters.
-
-        ienv_path : Path
-            path to the irods_environment.json to create a new session.
-        logger : logging.Logger
-            Logger
-        ops : ibridges.Opertions
-            Defines the data and metadata operations to perform. This thread currently uses:
-            create_dir, create_collection, upload, download and execute_meta_download
-            Please refer to the iBridges documentation: https://ibridges.readthedocs.io/
-        """
-        super().__init__()
-
-        self.logger = logger
-        self.thread_session = Session(irods_env=ienv_path)
-        self.logger.debug("Transfer data thread: Created new session.")
+    def __init__(self, ienv_path: Path, logger, ops, overwrite: bool):
+        super().__init__(logger, ienv_path)
         self.ops = ops
         self.overwrite = overwrite
 
-        self.up_sizes = sum(lpath.stat().st_size for lpath, _ in self.ops.upload)
-        self.down_sizes = sum(ipath.size for ipath, _ in self.ops.download)
+        self.up_sizes = sum(l.stat().st_size for l, _ in ops.upload)
+        self.down_sizes = sum(i.size for i, _ in ops.download)
 
-    def _delete_session(self):
-        self.thread_session.close()
-        if self.thread_session.irods_session is None:
-            self.logger.debug("Transfer data thread: Thread session successfully deleted.")
-        else:
-            self.logger.debug("Transfer data thread: Thread session still exists.")
-
-    def run(self):
-        """Run the thread."""
+    # ------------------------------
+    # Upload helpers
+    # ------------------------------
+    def _upload_files(self, transfer_out):
+        transferred = 0
         obj_count = 0
         obj_failed = 0
-        file_count = 0
-        file_failed = 0
-        transfer_out = {}
-        transfer_out["error"] = ""
-        transferred_size = 0
-
-        self.ops.execute_create_coll(self.thread_session)
-        self.ops.execute_create_dir()
 
         for local_path, irods_path in self.ops.upload:
             try:
+                from ibridges.executor import _obj_put
                 _obj_put(
                     self.thread_session,
                     local_path,
@@ -126,33 +109,31 @@ class TransferDataThread(PySide6.QtCore.QThread):
                     options=self.ops.options,
                     resc_name=self.ops.resc_name,
                 )
-                transferred_size += local_path.stat().st_size
+                transferred += local_path.stat().st_size
                 obj_count += 1
-                self.logger.info(
-                    "Transfer data thread: Transfer %s -->  %s, overwrite %s",
-                    local_path,
-                    irods_path,
-                    self.overwrite,
-                )
-            except Exception as error:
+                self.logger.info("Upload %s → %s", local_path, irods_path)
+
+            except Exception as exc:
                 obj_failed += 1
-                self.logger.exception(
-                    "Transfer data thread: Could not transfer  %s --> %s; %s",
-                    local_path,
-                    irods_path,
-                    repr(error),
-                )
-                transfer_out["error"] = (
-                    transfer_out["error"]
-                    + f"\nTransfer failed, cannot upload {str(local_path)}: {repr(error)}"
-                )
+                msg = f"Upload failed for {local_path}: {repr(exc)}"
+                self.logger.exception(msg)
+                transfer_out["error"] += "\n" + msg
+
             self.current_progress.emit(
-                [self.up_sizes, transferred_size, obj_count, len(self.ops.upload), obj_failed, ""]
+                [self.up_sizes, transferred, obj_count, len(self.ops.upload), obj_failed, ""]
             )
 
-        transferred_size = 0
+    # ------------------------------
+    # Download helpers
+    # ------------------------------
+    def _download_files(self, transfer_out):
+        transferred = 0
+        file_count = 0
+        file_failed = 0
+
         for irods_path, local_path in self.ops.download:
             try:
+                from ibridges.executor import _obj_get
                 _obj_get(
                     self.thread_session,
                     irods_path,
@@ -161,147 +142,87 @@ class TransferDataThread(PySide6.QtCore.QThread):
                     resc_name=self.ops.resc_name,
                     options=self.ops.options,
                 )
+                transferred += irods_path.size
                 file_count += 1
-                transferred_size += irods_path.size
-                self.logger.info(
-                    "Transfer data thread: Transfer %s -->  %s, overwrite %s",
-                    irods_path,
-                    local_path,
-                    self.overwrite,
-                )
-            except Exception as error:
+                self.logger.info("Download %s → %s", irods_path, local_path)
+
+            except Exception as exc:
                 file_failed += 1
-                self.logger.exception(
-                    "Transfer data thread: Could not transfer  %s --> %s; %s",
-                    irods_path,
-                    local_path,
-                    repr(error),
-                )
-                transfer_out["error"] = (
-                    transfer_out["error"]
-                    + f"\nTransfer failed, cannot download {str(irods_path)}: {repr(error)}"
-                )
+                msg = f"Download failed for {irods_path}: {repr(exc)}"
+                self.logger.exception(msg)
+                transfer_out["error"] += "\n" + msg
+
             self.current_progress.emit(
-                [
-                    self.down_sizes,
-                    transferred_size,
-                    file_count,
-                    len(self.ops.download),
-                    file_failed,
-                    "",
-                ]
+                [self.down_sizes, transferred, file_count, len(self.ops.download), file_failed, ""]
             )
 
+    # ------------------------------
+    # Metadata helpers
+    # ------------------------------
+    def _metadata_operations(self, transfer_out):
         try:
             self.ops.execute_meta_download()
-        except Exception as error:
-            self.current_progress.emit(
-                [
-                    self.down_sizes,
-                    transferred_size,
-                    file_count,
-                    len(self.ops.download),
-                    file_failed,
-                    f"Metadata download failed. {repr(error)}",
-                ]
-            )
-            transfer_out["error"] = (
-                transfer_out["error"]
-                + f"\nTransfer failed, cannot get Metadata {str(irods_path)}: {repr(error)}"
-            )
-            self.logger.exception(
-                "Transfer data thread: Could not download metadata  %s --> %s; %s",
-                local_path,
-                irods_path,
-                repr(error),
-            )
+        except Exception as exc:
+            msg = f"Metadata download failed: {repr(exc)}"
+            self.logger.exception(msg)
+            transfer_out["error"] += "\n" + msg
+
         try:
             self.ops.execute_meta_upload()
-        except Exception as error:
-            self.current_progress.emit(
-                [
-                    self.up_sizes,
-                    transferred_size,
-                    file_count,
-                    len(self.ops.upload),
-                    file_failed,
-                    f"Metadata upload failed. {repr(error)}",
-                ]
-            )
-            transfer_out["error"] = (
-                transfer_out["error"]
-                + f"\nTransfer failed, cannot add Metadata {str(irods_path)}: {repr(error)}"
-            )
-            self.logger.exception(
-                "Transfer data thread: Could not upload metadata  %s --> %s; %s",
-                local_path,
-                irods_path,
-                repr(error),
-            )
-        self._delete_session()
-        self.result.emit(transfer_out)
+        except Exception as exc:
+            msg = f"Metadata upload failed: {repr(exc)}"
+            self.logger.exception(msg)
+            transfer_out["error"] += "\n" + msg
+
+    # ------------------------------
+    # Main run()
+    # ------------------------------
+    def run(self):
+        transfer_out = {"error": ""}
+
+        try:
+            self.ops.execute_create_coll(self.thread_session)
+            self.ops.execute_create_dir()
+
+            self._upload_files(transfer_out)
+            self._download_files(transfer_out)
+            self._metadata_operations(transfer_out)
+
+        finally:
+            self.cleanup_session()
+            self.result.emit(transfer_out)
 
 
-class SyncThread(PySide6.QtCore.QThread):
+class SyncThread(BaseThread):
     """Sync between iRODS and local FS."""
 
-    result = PySide6.QtCore.Signal(dict)
-
     def __init__(self, ienv_path, logger, source, target, dry_run: bool):
-        """Pass download parameters."""
-        super().__init__()
-
-        self.logger = logger
-        self.thread_session = Session(irods_env=ienv_path)
-        self.logger.debug("Sync thread: created new session")
+        super().__init__(logger, ienv_path)
         self.source = source
         self.target = target
         self.dry_run = dry_run
 
-    def _delete_session(self):
-        self.thread_session.close()
-        if self.thread_session.irods_session is None:
-            self.logger.debug("Sync thread: Thread session successfully deleted.")
-        else:
-            self.logger.debug("Sync thread: Thread session still exists.")
-
     def run(self):
-        """Run the thread."""
-        sync_out = {}
-        sync_out["error"] = ""
+        sync_out = {"error": ""}
 
         try:
+            from ibridges import sync
             result = sync(
                 self.source,
                 self.target,
                 dry_run=self.dry_run,
                 copy_empty_folders=True,
             )
-            self.logger.info(
-                "Sync %s to %s, dry_run %s", str(self.source), str(self.target), str(self.dry_run)
-            )
+            self.logger.info("Sync %s → %s (dry_run=%s)", self.source, self.target, self.dry_run)
+
             if self.dry_run:
                 sync_out["result"] = result
 
-        except PermissionError as error:
-            sync_out["error"] = f"Sync failed. No access to {error.filename}."
-            self.logger.exception(
-                "Sync failed: %s --> %s; %s", str(self.source), str(self.target), repr(error)
-            )
-        except CAT_NO_ACCESS_PERMISSION as error:
-            sync_out["error"] = (
-                "There is data in the iRODS collection which you are not allowed to access."
-            )
-            self.logger.exception(
-                "Sync failed: %s --> %s; %s", str(self.source), str(self.target), repr(error)
-            )
-        except Exception as error:
-            self.logger.exception(
-                "Sync failed: %s --> %s; %s", str(self.source), str(self.target), repr(error)
-            )
-            sync_out["error"] = (
-                sync_out["error"]
-                + f"\nSync failed: {str(self.source)} --> {str(self.target)}: {repr(error)}"
-            )
-        self._delete_session()
-        self.result.emit(sync_out)
+        except Exception as exc:
+            msg = f"Sync failed: {repr(exc)}"
+            self.logger.exception(msg)
+            sync_out["error"] = msg
+
+        finally:
+            self.cleanup_session()
+            self.result.emit(sync_out)
