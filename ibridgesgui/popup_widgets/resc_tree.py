18d512d23fd0a6c99239f83c11e2196961786bf8
chstaiger <staigerchristine@gmail.com>
2026-05-27 20:26:06 +0200

    fix resource info

--- ibridgesgui/popup_widgets/resc_tree.py
+++ ibridgesgui/popup_widgets/resc_tree.py
@@ -1,8 +1,8 @@
 """Popup to show resource tree."""
 from ibridges.resources import Resources
-from PySide6 import QtCore
-from PySide6.QtGui import QStandardItem, QStandardItemModel
-from PySide6.QtWidgets import QDialog, QHeaderView
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtGui import QStandardItemModel
+from PySide6.QtWidgets import QDialog, QHeaderView, QTreeWidgetItem
 
 from ibridgesgui.popup_widgets.base import UiDialogMixin
 from ibridgesgui.ui_files.rescTree import Ui_rescTree
@@ -19,6 +19,11 @@ class RescInfoDialog(QDialog, Ui_rescTree, UiDialogMixin):
         self._init_ui()
         self.setWindowTitle("Resource Tree")
 
+        self.resc_view.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding,
+            QtWidgets.QSizePolicy.Expanding
+        )
+
         self.rescs = Resources(session)
         self.ok_button.clicked.connect(self.accept)
 
@@ -26,14 +31,24 @@ class RescInfoDialog(QDialog, Ui_rescTree, UiDialogMixin):
 
     def _adjust_columns(self):
         header = self.resc_view.header()
+    
+        # Allow columns to shrink below Qt's default minimum
+        header.setMinimumSectionSize(0)
+    
+        # IMPORTANT: override Qt's default behavior for the LAST column
+        header.setSectionResizeMode(2, QHeaderView.Fixed)
+        self.resc_view.setColumnWidth(2, 20)
+    
+        # Now set column 1
+        header.setSectionResizeMode(1, QHeaderView.Fixed)
+        self.resc_view.setColumnWidth(1, 60)
+    
+        # Finally set column 0 to stretch
         header.setSectionResizeMode(0, QHeaderView.Stretch)
-        header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
         self.resc_view.setColumnWidth(0, 300)
 
     def _populate_tree(self):
-        model = QStandardItemModel()
-        model.setHorizontalHeaderLabels(["Resource (Type)", "Free Space", "Status"])
-
+        self.resc_view.clear()
         resc_dict = self.rescs.resources()
 
         # Build ID → name mapping
@@ -51,11 +66,11 @@ class RescInfoDialog(QDialog, Ui_rescTree, UiDialogMixin):
             size = fields.get("free_space", 0)
             status = fields.get("status", "")
 
-            items[name] = [
-                    QStandardItem(display_name),
-                    QStandardItem(str(size)),
-                    QStandardItem("" if status is None else str(status))
-                    ]
+            items[name] = QTreeWidgetItem([
+                display_name,
+                str(size),
+                "" if status is None else str(status)
+            ])
 
         roots = [r[0] for r in self.rescs.root_resources]
 
@@ -64,12 +79,12 @@ class RescInfoDialog(QDialog, Ui_rescTree, UiDialogMixin):
             if parent_id:
                 parent_name = id_to_name.get(str(parent_id))
                 if parent_name in items:
-                    items[parent_name][0].appendRow(items[name])
+                    items[parent_name].addChild(items[name])
 
+        # Add root items
         for root in roots:
-            model.appendRow(items[root])
-
-        self.resc_view.setModel(model)
+            self.resc_view.addTopLevelItem(items[root])
+        
         self.resc_view.expandAll()
         QtCore.QTimer.singleShot(0, self._adjust_columns)
         self.error_label.clear()
@@ -81,3 +96,4 @@ class RescInfoDialog(QDialog, Ui_rescTree, UiDialogMixin):
         super().showEvent(event)
         self.error_label.setText("Loading…")
         QtCore.QTimer.singleShot(0, self._populate_tree)
+        QtCore.QTimer.singleShot(50, self._populate_tree)
