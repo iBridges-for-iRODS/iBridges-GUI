3d31cf99179dd8909fb183d18d6d1d20130ae2c4
Staiger, Christine <staigerchristine@gmail.com>
2026-07-29 18:52:14 +0200

    account for that not each irods session carries an env file

--- ibridgesgui/gui_utils.py
+++ ibridgesgui/gui_utils.py
@@ -160,7 +160,9 @@ def prep_session_for_copy(session, error_label) -> Optional[Path]:
     last_env_path = Path.home() / ".irods" / get_last_ienv_path()
     session_env_path = session.irods_session.env_file
 
-    if last_env_path != session_env_path:
+    # irods session does not always contain the env path.
+    # this is the case when the session is created with a password
+    if session_env_path and (last_env_path != session_env_path):
         error_label.setText(
             "A different iRODS configuration was loaded outside this app. "
             "Restart the session to continue."
